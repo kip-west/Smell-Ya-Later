fb80212074c025aece527321a896859aa123566f
Jake West <jacobkwest93@gmail.com>
2020-09-17 13:22:50 -0600

    Add styling to Item card #5

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,8 +4,8 @@ import './Item.css'
 const Item = () => {
     return(
         <div className='item-card'>
-            <h1>Candle</h1>
-            <img src='https://images.unsplash.com/photo-1545659446-f03b9dc0827a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' alt='Candle' width='250px'/>
+            <img className='item-image' src='https://images.unsplash.com/photo-1545659446-f03b9dc0827a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' alt='Candle' />
+            <h1 className='item-name'>Candle</h1>
         </div>
     )
 }
