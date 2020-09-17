e5db022c0aac96295c7bc3b68f7d178fe1818a70
Jake West <jacobkwest93@gmail.com>
2020-09-17 15:23:15 -0600

    Add implementation code for Header unit test #3

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div className='header'>
+            <h1>Smell Ya Later</h1>
+            <button>Home</button>
+            <button>User Profile</button>
+            <button>Activity</button>
+        </div>
+    )
+}
+
+export default Header
