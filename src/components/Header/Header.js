2f303a69f74117d0afd838b249e7489169701a26
Jake West <jacobkwest93@gmail.com>
2020-09-17 15:39:47 -0600

    Add Header styling #3

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return(
         <div className='header'>
             <h1>Smell Ya Later</h1>
-            <button>Home</button>
-            <button>User Profile</button>
-            <button>Activity</button>
+            <div className='links-container'>
+                <button>Home</button>
+                <button>User Profile</button>
+                <button>Activity</button>
+            </div>
         </div>
     )
 }
