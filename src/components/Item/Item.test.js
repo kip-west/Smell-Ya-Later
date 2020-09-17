f8644c93621b201998480c517f0d3d6b52d52997
Jake West <jacobkwest93@gmail.com>
2020-09-17 14:51:39 -0600

    Refactored Item tests #7

--- src/components/Item/Item.test.js
+++ src/components/Item/Item.test.js
@@ -4,7 +4,9 @@ import { render, screen } from '@testing-library/react'
 
 describe('Item', () => {
     it('Should render the correct content on display', () => {
-        render(<Item />)
+        render(
+            <Item id={999} name='Candle' imageUrl='www.image.candle.png' />
+        )
 
         const itemName = screen.getByRole('heading', { name: 'Candle' })
         const image = screen.getByAltText('Candle')
