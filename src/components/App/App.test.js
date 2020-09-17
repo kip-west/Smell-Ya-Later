65769739064a5847ebe4168d95b50dfe538b59fa
Jake West <jacobkwest93@gmail.com>
2020-09-17 17:01:44 -0600

    Add unit test for App content on load #12

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import Item from './components/Item/Item';
 
 describe('App', () => {
-  it('should render the correct content', () => {
-    
+  it('Should render the correct content on page load', () => {
+    render(<App />)
+
+    const headerText = screen.getByRole('heading', { name: 'Smell Ya Later' })
+    const homeButton = screen.getByRole('link', { name: 'Home' })
+    const userProfileButton = screen.getByRole('link', { name: 'User Profile' })
+    const activityButton = screen.getByRole('link', { name: 'Activity' })
+    const smellKitHeading = screen.getByRole('heading', { name: 'Your Smell Kit' })
+
+    expect(headerText).toBeInTheDocument();
+    expect(homeButton).toBeInTheDocument();
+    expect(userProfileButton).toBeInTheDocument();
+    expect(activityButton).toBeInTheDocument();
+    expect(smellKitHeading).toBeInTheDocument();
   })
 })
