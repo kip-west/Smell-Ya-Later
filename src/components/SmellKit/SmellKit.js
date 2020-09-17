a0a300614601c16d27e6cfb2af8ae37c0edf6def
Jake West <jacobkwest93@gmail.com>
2020-09-17 14:40:00 -0600

    Add more items to state & re-style item cards

--- src/components/SmellKit/SmellKit.js
+++ src/components/SmellKit/SmellKit.js
@@ -17,7 +17,17 @@ class SmellKit extends Component {
                     id: 2,
                     name: 'Lemon',
                     image: 'https://images.unsplash.com/photo-1568569350062-ebfa3cb195df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-                }
+                },
+                3: {
+                    id: 3,
+                    name: 'Rosemary',
+                    image: 'https://images.unsplash.com/photo-1515586000433-45406d8e6662?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                4: {
+                    id: 4,
+                    name: 'Eucalyptus',
+                    image: 'https://images.unsplash.com/photo-1517008607120-139741f5eeef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
             }
         }
     }
