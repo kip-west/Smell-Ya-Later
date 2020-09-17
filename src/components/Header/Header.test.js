ad29952d8ae3c91a1de24546ca237755470ad833
Jake West <jacobkwest93@gmail.com>
2020-09-17 16:11:58 -0600

    Refactor unit test for Header #3

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
 import Header from './Header'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('Header', () => {
     it('Should render the correct content', () => {
-        render(<Header />)
+        render(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>
+        )
 
         const headerText = screen.getByRole('heading', { name: 'Smell Ya Later' })
-        const homeButton = screen.getByRole('button', { name: 'Home' })
-        const userProfileButton = screen.getByRole('button', { name: 'User Profile' })
-        const activityButton = screen.getByRole('button', { name: 'Activity' })
+        const homeButton = screen.getByRole('link', { name: 'Home' })
+        const userProfileButton = screen.getByRole('link', { name: 'User Profile' })
+        const activityButton = screen.getByRole('link', { name: 'Activity' })
 
         expect(headerText).toBeInTheDocument();
         expect(homeButton).toBeInTheDocument();
