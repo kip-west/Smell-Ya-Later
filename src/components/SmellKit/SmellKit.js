157541714578ba00d11785e91149076125b4a71d
Jake West <jacobkwest93@gmail.com>
2020-09-17 13:57:05 -0600

    Add state to SmellKit with a hard coded item #4

--- src/components/SmellKit/SmellKit.js
+++ src/components/SmellKit/SmellKit.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 class SmellKit extends Component {
     constructor() {
         super();
+        this.state = {
+            items: {
+                1: { 
+                    id: 1,
+                    name: 'Candle',
+                    image: 'https://images.unsplash.com/photo-1545659446-f03b9dc0827a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                }
+            }
+        }
     }
     
     render() {
