514cdddde75bb1d50f4b4efd871eb87a143b7223
Jake West <jacobkwest93@gmail.com>
2020-09-17 17:49:35 -0600

    Add implementation code for UserProfile form & render on App #10

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css'
 import SmellKit from '../SmellKit/SmellKit'
 import Header from '../Header/Header'
+import UserProfile from '../UserProfile/UserProfile'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <BrowserRouter>
           <Header />
           <Route exact path='/' render={() => <SmellKit />}/>
-          <Route exact path='/user-profile' render={() => <h1>User Profile!</h1>} />
+          <Route exact path='/user-profile' render={() => <UserProfile />} />
           <Route exact path='/activity' render={() => <h1>Recent Activity!</h1>} />
         </BrowserRouter>
     </main>
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,7 +4,14 @@ import './UserProfile.css'
 class UserProfile extends Component {
     render() {
         return (
-            <h1>User Profile!</h1>
+            <div className='user-profile-container'>
+                <h1>User Profile</h1>
+                <form className='update-profile-form'>
+                    <input name='name-input' placeholder='Enter name here' />
+                    <input name='phone-input' placeholder='Enter phone number here' />
+                    <button className='update-profile-button'>Submit</button>
+                </form>
+            </div>
         )
     }
 }
