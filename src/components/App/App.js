f700289f64fd82dd263ee80706c531c1e0097313
Jake West <jacobkwest93@gmail.com>
2020-09-17 16:54:41 -0600

    Add App style sheet & link to App Component #12

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import SmellKit from '../SmellKit/SmellKit'
 import Header from '../Header/Header'
 import { BrowserRouter, Route } from 'react-router-dom'
