684947421042630e6d423a88ded18491aa62f32e
Jake West <jacobkwest93@gmail.com>
2020-09-17 13:31:01 -0600

    Renders an Item in App #5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Item from './components/Item/Item'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Smell Ya Later</h1>
+        <Item />
       </header>
     </div>
   );
