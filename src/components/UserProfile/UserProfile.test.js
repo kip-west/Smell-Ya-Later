5a0dd98e4050f563a57444bfe751d0c6ad080a24
Jake West <jacobkwest93@gmail.com>
2020-09-17 17:48:34 -0600

    Add unit test code for update profile form #10

--- src/components/UserProfile/UserProfile.test.js
+++ src/components/UserProfile/UserProfile.test.js
@@ -6,8 +6,20 @@ describe('User Profile', () => {
     it('Should display correct content when rendered', () => {
         render(<UserProfile />)
 
-        const userProfileHeading = screen.getByRole('heading', { name: 'User Profile!' })
+        const userProfileHeading = screen.getByRole('heading', { name: 'User Profile' })
 
         expect(userProfileHeading).toBeInTheDocument();
+    })
+
+    it('Should have a form where users can update their information', () => {
+        render(<UserProfile />)
+
+        const nameInput = screen.getByPlaceholderText('Enter name here')
+        const phoneNumberInput = screen.getByPlaceholderText('Enter phone number here')
+        const submitButton = screen.getByRole('button', { name: 'Submit' })
+
+        expect(nameInput).toBeInTheDocument()
+        expect(phoneNumberInput).toBeInTheDocument()
+        expect(submitButton).toBeInTheDocument()
     })
 })
