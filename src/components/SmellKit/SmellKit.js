f42b007cf22d9b2ca58658f4e237d937b10d3e3d
Jake West <jacobkwest93@gmail.com>
2020-09-17 13:45:38 -0600

    Add SmellKit Component and return a header #4

---
+++ src/components/SmellKit/SmellKit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SmellKit = () => {
+    return(
+        <div className='smell-kit'>
+            <h1>Your Smell Kit</h1>
+        </div>
+    )
+}
+
+export default SmellKit
