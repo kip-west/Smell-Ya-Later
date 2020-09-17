7ef3ab7c786fa61571766c1efd0a58bf4ab4bc96
Jake West <jacobkwest93@gmail.com>
2020-09-17 16:07:36 -0600

    Add Route for Home, User Profile, and Activity (latter two have dummy content) #3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import SmellKit from './components/SmellKit/SmellKit'
 import Header from './components/Header/Header'
-import Router from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
     <main className="App">
-        <Header />
-        <SmellKit />
+        <BrowserRouter>
+          <Header />
+          <Route exact path='/' render={() => <SmellKit />}/>
+          <Route exact path='/user-profile' render={() => <h1>User Profile!</h1>} />
+          <Route exact path='/activity' render={() => <h1>Recent Activity!</h1>} />
+        </BrowserRouter>
     </main>
   );
 }
