8d8b00ac3aa39ac12755c46c511deafd15e287d3
Jake West <jacobkwest93@gmail.com>
2020-09-17 13:50:23 -0600

    Refactor SmellKit to be a class component #4

--- src/components/SmellKit/SmellKit.js
+++ src/components/SmellKit/SmellKit.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SmellKit = () => {
-    return(
-        <div className='smell-kit'>
-            <h1>Your Smell Kit</h1>
-        </div>
-    )
+class SmellKit extends Component {
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return(
+            <div className='smell-kit'>
+                <h1>Your Smell Kit</h1>
+            </div>
+        )
+    }
 }
 
 export default SmellKit
