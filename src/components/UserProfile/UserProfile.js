24a7e65058e1920d83333d1a811acc709b38174f
Jake West <jacobkwest93@gmail.com>
2020-09-17 18:57:47 -0600

    Add clearForm & handleSubmit methods #10

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -18,13 +18,31 @@ class UserProfile extends Component {
         this.setState( { [event.target.name] : event.target.value })
     }
 
+    clearForm = () => {
+        const nameInput = document.querySelector('.name-input');
+        const phoneInput = document.querySelector('.phone-input')
+        nameInput.value = '';
+        phoneInput.value = '';
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.setState( { 
+            userInfo: {
+                name: this.state.nameInput,
+                phoneNumber: this.state.phoneNumberInput
+            }
+        })
+        this.clearForm()
+    }
+
     render() {
         return (
             <div className='user-profile-container'>
                 <h1>User Profile</h1>
                 <div className='form-container'>
                     <p>Update Contact Information</p>
-                    <form className='update-profile-form'>
+                    <form className='update-profile-form' onSubmit={this.handleSubmit}>
                         <input className='name-input' name='nameInput' placeholder='Enter name here' onChange={this.handleChange} />
                         <input className='phone-input' name='phoneNumberInput' type='tel' placeholder='Enter phone number here' onChange={this.handleChange} />
                         <button className='update-profile-button'>Submit</button>
