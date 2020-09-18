5e2d12453baad1bcd86fb34a66fdae147ef72445
Jake West <jacobkwest93@gmail.com>
2020-09-17 18:38:07 -0600

    Add unit test for Form values updating on change #10

--- src/components/UserProfile/UserProfile.test.js
+++ src/components/UserProfile/UserProfile.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import UserProfile from './UserProfile'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 describe('User Profile', () => {
     it('Should display correct content when rendered', () => {
@@ -11,15 +11,30 @@ describe('User Profile', () => {
         expect(userProfileHeading).toBeInTheDocument();
     })
 
-    it('Should have a form where users can update their information', () => {
-        render(<UserProfile />)
+    describe('Update Contact Information Form', () => {
+        it('Should have a form where users can update their information', () => {
+            render(<UserProfile />)
+    
+            const nameInput = screen.getByPlaceholderText('Enter name here')
+            const phoneNumberInput = screen.getByPlaceholderText('Enter phone number here')
+            const submitButton = screen.getByRole('button', { name: 'Submit' })
+    
+            expect(nameInput).toBeInTheDocument()
+            expect(phoneNumberInput).toBeInTheDocument()
+            expect(submitButton).toBeInTheDocument()
+        })
 
-        const nameInput = screen.getByPlaceholderText('Enter name here')
-        const phoneNumberInput = screen.getByPlaceholderText('Enter phone number here')
-        const submitButton = screen.getByRole('button', { name: 'Submit' })
+        it('Should update it\'s own values on change', () => {
+            render(<UserProfile />)
 
-        expect(nameInput).toBeInTheDocument()
-        expect(phoneNumberInput).toBeInTheDocument()
-        expect(submitButton).toBeInTheDocument()
+            const nameInput = screen.getByPlaceholderText('Enter name here')
+            const phoneInput = screen.getByPlaceholderText('Enter phone number here')
+
+            fireEvent.change(nameInput, { target: { value:  'Jake'} })
+            fireEvent.change(phoneInput, { target: { value: '+1(555)-555-5555' }})
+
+            expect(nameInput.value).toBe('Jake')
+            expect(phoneInput.value).toBe('+1(555)-555-5555')
+        })
     })
 })
