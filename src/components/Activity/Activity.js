15df863b1d275e4545a1b362a6635331edf31ee7
Jake West <jacobkwest93@gmail.com>
2020-09-19 13:13:47 -0600

    Add state & componentDidMount to Activity to getAllMessages #20

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import './Activity.css'
+import { getAllMessages } from '../../apiCalls'
 
 class Activity extends Component {
     constructor() {
         super();
         this.state = {
+            messages: {},
+            error: ''
+        }
+    }
 
+    async componentDidMount() {
+        try {
+            const data = await getAllMessages()
+            this.setState({ messages: data.messages })
+        } catch (error) {
+            this.setState({ error: error })
         }
     }
 
