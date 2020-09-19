3e4db70eed4995382c2c87caf123a1e43a13bfdf
Jake West <jacobkwest93@gmail.com>
2020-09-19 07:21:42 -0600

    Add unit test for send text button & make network request functionality #18

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,6 +6,7 @@ const Item = ({ id, name, imageUrl }) => {
         <div className='item-card' id={id}>
             <img className='item-image' src={imageUrl} alt={name} />
             <h1 className='item-name'>{name}</h1>
+            <button name={name} onClick={handleClick}>Send Text</button>
         </div>
     )
 }
--- src/components/Item/Item.test.js
+++ src/components/Item/Item.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Item from './Item'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { sendMessage } from '../../apiCalls'
+jest.mock('../../apiCalls')
 
 describe('Item', () => {
     it('Should render the correct content on display', () => {
@@ -10,8 +12,23 @@ describe('Item', () => {
 
         const itemName = screen.getByRole('heading', { name: 'Candle' })
         const image = screen.getByAltText('Candle')
+        const button = screen.getByRole('button', { name: 'Send Text' })
 
         expect(itemName).toBeInTheDocument();
         expect(image).toBeInTheDocument();
+        expect(button).toBeInTheDocument();
+    })
+
+    it('Should make a network request when a item is clicked on', () => {
+        render(<Item id={999} name='Candle' imageUrl='www.image.candle.png' />)
+
+        const button = screen.getByRole('button', { name: 'Send Text' })
+        fireEvent.click(button)
+
+        expect(sendMessage).toBeCalledTimes(1)
+        expect(sendMessage).toBeCalledWith({
+            "to": "+19102625254",
+            "body": "Candle"
+        })
     })
 })
