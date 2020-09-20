98d36a4e6ef5c03a3c506a227890418a0ea97fee
Jake West <jacobkwest93@gmail.com>
2020-09-19 20:01:40 -0600

    Add unit test for ActivityDetails component #21

--- src/components/ActivityDetails/ActivityDetails.test.js
+++ src/components/ActivityDetails/ActivityDetails.test.js
@@ -1,8 +1,19 @@
 import react from 'react'
 import ActivityDetails from './ActivityDetails'
+import { render } from '@testing-library/react'
 
 describe('ActivityDetails', () => {
     it('Should render the correct content', () => {
-        
+        render(<ActivityDetails item='Lemon' rating={3} date='2020/01/01' />)
+
+        const heading = screen.getByRole('heading', { name: 'Lemon' })
+        const rating = screen.getByRole('heading', { name: 'Your Rating: 3' })
+        const date = screen.getByRole('heading', { name: '2020/01/01' })
+        const deleteButton = screen.getByRole('button', { name: 'Delete' })
+
+        expect(heading).toBeInTheDocument()
+        expect(rating).toBeInTheDocument()
+        expect(date).toBeInTheDocument()
+        expect(deleteButton).toBeInTheDocument()
     })
 })
