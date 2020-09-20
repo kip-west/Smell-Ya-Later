ae9c89e82461a479c64e6c554328eb1e44fc8391
Jake West <jacobkwest93@gmail.com>
2020-09-20 17:11:20 -0600

    Add state to app for user & create method to update state #25

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import SmellKit from '../SmellKit/SmellKit'
 import Header from '../Header/Header'
 import UserProfile from '../UserProfile/UserProfile'
 import Activity from '../Activity/Activity'
 import { BrowserRouter, Route } from 'react-router-dom'
-import Collapsible from 'react-collapsible'
 
-function App() {
-  return (
-    <main className="App">
-        <BrowserRouter>
-          <Header />
-          <Route exact path='/' render={() => <SmellKit />} />
-          <Route exact path='/user-profile' render={() => <UserProfile />} />
-          <Route exact path='/activity' render={() => <Activity />} />
-        </BrowserRouter>
-    </main>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        name: '',
+        phoneNumber: ''
+      }
+    }
+  }
+
+  updateUserInformation = (user) => {
+    console.log(user)
+    this.setState ( { user: user })
+  }
+
+  render() {
+    return (
+      <main className="App">
+          <BrowserRouter>
+            <Header />
+            <Route exact path='/' render={() => <SmellKit user={this.state.user} />} />
+            <Route exact path='/user-profile' render={() => <UserProfile updateProfile={this.updateUserInformation} user={this.state.user} />} />
+            <Route exact path='/activity' render={() => <Activity />} />
+          </BrowserRouter>
+      </main>
+    )
+  }
 }
 
 export default App;
