d6fd31d76f2fe4d803223925b814a937ddaf5b3f
Jake West <jacobkwest93@gmail.com>
2020-09-20 09:35:30 -0600

    Add nose icon & render in header #24

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css'
 import { NavLink } from 'react-router-dom'
+import noseIcon from '../../assets/nose-icon.png'
 
 const Header = () => {
     return(
         <div className='header'>
-            <NavLink to='/' className='main-header-link' style={{ textDecoration: 'none' }}><h1>Smell Ya Later</h1></NavLink>
+            <NavLink to='/' className='main-header-link' style={{ textDecoration: 'none' }}>
+                <img className='icon' alt='Home icon' src={noseIcon} /><h1>Smell Ya Later</h1></NavLink>
             <div className='links-container'>
                 <NavLink to='/' className='header-link' style={{ textDecoration: 'none' }}>Home</NavLink>
                 <NavLink to='/user-profile' className='header-link' style={{ textDecoration: 'none' }}>User Profile</NavLink>
