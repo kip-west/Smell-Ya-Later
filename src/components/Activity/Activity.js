ce627e76e3bf085fac582f64296cd547870bfd86
Jake West <jacobkwest93@gmail.com>
2020-09-19 20:35:00 -0600

    Refactor createLists method & set up details sections #21

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Activity.css'
 import { getAllMessages } from '../../apiCalls'
+import ActivityDetails from '../ActivityDetails/ActivityDetails'
 
 class Activity extends Component {
     constructor() {
@@ -28,7 +29,6 @@ class Activity extends Component {
         const splitBody = body.split(' ')
         const lastCharacter = parseInt(splitBody[1])
         const firstWord = splitBody[0]
-        console.log(firstWord, lastCharacter)
         const items = ['Candle', 'Eucalyptus', 'Lemon', 'Rosemary']
         if(items.includes(firstWord)) {
             return(lastCharacter > 0 && lastCharacter < 6)
@@ -43,21 +43,37 @@ class Activity extends Component {
     }
 
     createList(messages) {
-        return messages.map(message => {
-            if(this.checkMessageBody(message)) {
-                return(<li>{message.body}, {message.dateCreated}</li>)
-            }
+        const filteredMessages = messages.filter(message => this.checkMessageBody(message))
+        return filteredMessages.map(message => {
+            const body = message.body.split(' ')
+            return(<ActivityDetails item={body[0]} rating={body[1]} date={message.dateCreated} />)
         })
     }
 
     render() {
+        const ratings = this.createList(this.filterInboundMessages())
         return(
             <div className='Activity-container'>
                 <h1>Recent Activity</h1>
                 {this.state.messages && 
-                    <ul>
-                        {this.createList(this.filterInboundMessages())}
-                    </ul>
+                <div className='ratings-container'>
+                    <div className='lemon-ratings'>
+                        <h1>Lemon</h1>
+                        {ratings.filter(rating => rating.props.item === 'Lemon')}
+                    </div>
+                    <div className='candle-ratings'>
+                        <h1>Candle</h1>
+                        {ratings.filter(rating => rating.props.item === 'Candle')}
+                    </div>
+                    <div className='eucalyptus-ratings'>
+                        <h1>Eucalyptus</h1>
+                        {ratings.filter(rating => rating.props.item === 'Eucalyptus')}
+                    </div>
+                    <div className='rosemary-ratings'>
+                        <h1>Rosemary</h1>
+                        {ratings.filter(rating => rating.props.item === 'Rosemary')}
+                    </div>
+                </div>
                 }
             </div>
         )
