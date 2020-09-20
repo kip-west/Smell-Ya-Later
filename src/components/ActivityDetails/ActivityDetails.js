2c9a5a77d462c6f20409c5d17e33d4ae727a4da1
Jake West <jacobkwest93@gmail.com>
2020-09-19 19:56:31 -0600

    Add Activity Details boilerplate #21

---
+++ src/components/ActivityDetails/ActivityDetails.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import './ActivityDetails.css'
+
+export default ActivityDetails
---
+++ src/components/ActivityDetails/ActivityDetails.test.js
@@ -0,0 +1,8 @@
+import react from 'react'
+import ActivityDetails from './ActivityDetails'
+
+describe('ActivityDetails', () => {
+    it('Should render the correct content', () => {
+        
+    })
+})
