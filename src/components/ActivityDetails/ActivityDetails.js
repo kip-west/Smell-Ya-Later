1a146fca27217df4ed749eb1c7cbaba7dc464ae4
Jake West <jacobkwest93@gmail.com>
2020-09-19 20:35:44 -0600

    Remove header from each ActivityDetail & add styling for ratings containers #21

--- src/components/ActivityDetails/ActivityDetails.js
+++ src/components/ActivityDetails/ActivityDetails.js
@@ -4,7 +4,6 @@ import './ActivityDetails.css'
 const ActivityDetails = ({ item, rating, date }) => {
     return(
         <div className='Activity-Details'>
-            <h1>{item}</h1>
             <h2>Your Rating: {rating}</h2>
             <h3>on {date}</h3>
             <button className='delete-button'>Delete</button>
