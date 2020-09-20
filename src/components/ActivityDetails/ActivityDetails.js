baa9c6f46bacd71752270a0308a444b5adfab477
Jake West <jacobkwest93@gmail.com>
2020-09-20 08:09:52 -0600

    Re-style date/time on Activity Detail cards #24

--- src/components/ActivityDetails/ActivityDetails.js
+++ src/components/ActivityDetails/ActivityDetails.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './ActivityDetails.css'
+import moment from 'moment'
 
 const ActivityDetails = ({ id, rating, date }) => {
+    console.log(date)
     return(
         <div className='Activity-Details' id={id}>
             <h2 className='rating'>Your Rating: {rating}<span className="close" id={id}>&times;</span></h2>
-            <h3 className='date'>from {date}</h3>
+            <h3 className='date'>from {moment(date).format('dddd')},</h3>
+            <h3 className='date'>{moment(date.props.children).format('ll')}</h3>
+            <h3 className='date'>at {moment(date.props.children).format('LT')}</h3>
         </div>
     )
 }
