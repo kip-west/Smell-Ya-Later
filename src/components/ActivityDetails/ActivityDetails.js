8d4e2332b055773dbd30210edf9b3b2dfbc288e8
Jake West <jacobkwest93@gmail.com>
2020-09-19 20:06:19 -0600

    Add Activity Details implementation code #21

--- src/components/ActivityDetails/ActivityDetails.js
+++ src/components/ActivityDetails/ActivityDetails.js
@@ -1,4 +1,15 @@
 import React from 'react'
 import './ActivityDetails.css'
 
+const ActivityDetails = ({ item, rating, date }) => {
+    return(
+        <div className='Activity-Details'>
+            <h1>{item}</h1>
+            <h2>Your Rating: {rating}</h2>
+            <h3>on {date}</h3>
+            <button className='delete-button'>Delete</button>
+        </div>
+    )
+}
+
 export default ActivityDetails
--- src/components/ActivityDetails/ActivityDetails.test.js
+++ src/components/ActivityDetails/ActivityDetails.test.js
@@ -1,6 +1,6 @@
-import react from 'react'
+import React from 'react'
 import ActivityDetails from './ActivityDetails'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 describe('ActivityDetails', () => {
     it('Should render the correct content', () => {
@@ -8,7 +8,7 @@ describe('ActivityDetails', () => {
 
         const heading = screen.getByRole('heading', { name: 'Lemon' })
         const rating = screen.getByRole('heading', { name: 'Your Rating: 3' })
-        const date = screen.getByRole('heading', { name: '2020/01/01' })
+        const date = screen.getByRole('heading', { name: 'on 2020/01/01' })
         const deleteButton = screen.getByRole('button', { name: 'Delete' })
 
         expect(heading).toBeInTheDocument()
