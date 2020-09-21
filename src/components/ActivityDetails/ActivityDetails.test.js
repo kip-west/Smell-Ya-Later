d20d222ced35bf5456c160791ea7710193965afb
Jake West <jacobkwest93@gmail.com>
2020-09-21 14:54:16 -0600

    Add unit test for delete button & test for delete button click #31

--- src/components/ActivityDetails/ActivityDetails.test.js
+++ src/components/ActivityDetails/ActivityDetails.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ActivityDetails from './ActivityDetails'
 import Moment from 'react-moment'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 describe('ActivityDetails', () => {
     it('Should render the correct content', () => {
@@ -9,8 +9,26 @@ describe('ActivityDetails', () => {
         
         const rating = screen.getByText('Your Rating: 3', { exact: false })
         const date = screen.getByRole('heading', { name: 'Monday, Jan 1, 2020 at 9:03 AM' })
+        const deleteButton = screen.getByRole('button')
 
         expect(rating).toBeInTheDocument()
         expect(date).toBeInTheDocument()
+        expect(deleteButton).toBeInTheDocument()
+    })
+
+    it('Should call a method when the delete button is clicked', () => {
+        const mockDeleteActivityDetail = jest.fn()
+        render(<ActivityDetails id='999' item='Lemon' rating='3' date ={<Moment>{ '2020-01-01T16:03:33.000Z'}</Moment>} deleteActivityDetail={mockDeleteActivityDetail}/>)
+
+        const rating = screen.getByText('Your Rating: 3', { exact: false })
+        const date = screen.getByRole('heading', { name: 'Monday, Jan 1, 2020 at 9:03 AM' })
+        const deleteButton = screen.getByRole('button')
+        expect(rating).toBeInTheDocument()
+        expect(date).toBeInTheDocument()
+        expect(deleteButton).toBeInTheDocument()
+
+        fireEvent.click(deleteButton)
+        expect(mockDeleteActivityDetail).toBeCalledTimes(1)
+        expect(mockDeleteActivityDetail).toBeCalledWith('999')
     })
 })
