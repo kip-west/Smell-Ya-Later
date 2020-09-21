a052e0f9c47871b96398fa5755861ec8b7338e49
Jake West <jacobkwest93@gmail.com>
2020-09-21 11:52:23 -0600

    Remove extra screen.debug #30

--- src/components/ActivityDetails/ActivityDetails.test.js
+++ src/components/ActivityDetails/ActivityDetails.test.js
@@ -6,8 +6,7 @@ import { render, screen } from '@testing-library/react'
 describe('ActivityDetails', () => {
     it('Should render the correct content', () => {
         render(<ActivityDetails item='Lemon' rating='3' date={<Moment>{'2020-01-01T16:03:33.000Z'}</Moment>} />)
-
-        screen.debug()
+        
         const rating = screen.getByText('Your Rating: 3', { exact: false })
         const date = screen.getByRole('heading', { name: 'Monday, Jan 1, 2020 at 9:03 AM' })
 
